c801fe47f3e03dbfd5aa8da011f05e377be8157f
Your Name <jaknollmeyer@gmail.com>
2015-12-21 22:49:14 -0500

    Added method to randomly generate a transition from the list of transitions added via add_state

--- markov_vector_module.py
+++ markov_vector_module.py
@@ -1,4 +1,5 @@
 # defines a vector class to represent the states and transitions
+import random
 
 
 class markov_vector:
@@ -14,7 +15,17 @@ class markov_vector:
                 self.transitionCounts[state][transition] += 1
             else:
                 self.transitionCounts[state][transition] = 1
+            self.transitionsPerState[state] += 1
         else:
             self.transitionCounts[state] = dict({transition: 1})
-
+            self.transitionsPerState[state] = 1
     # function: generateTransition(state)
+
+    def generateTransition(self, state):
+        if state not in self.transitionCounts:
+            return None
+        while True:
+            for transition in self.transitionCounts[state]:
+                if (float(self.transitionCounts[state][transition]) /
+                   self.transitionsPerState[state]) > random.random():
+                    return transition
