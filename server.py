7abda2a7e2addf8f641a5113d410608b790b9840
Your Name <jaknollmeyer@gmail.com>
2015-12-24 16:28:16 -0500

    Form now works, will generate tweets from user submitted usernames

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from application_only_auth import Client
 import markov_vector_module
 app = Flask(__name__)
@@ -19,15 +19,15 @@ def static_proxy(path):
     return app.send_static_file(path)
 
 
-@app.route("/tweets")
+@app.route("/tweets", methods=['POST'])
 def tweets():
     text_corpus = ''
     maxid = None
     # paginate through the tweets, getting 200 at a time
-    for i in xrange(10):
+    for i in xrange(5):
         requestString = (
             'https://api.twitter.com/1.1/statuses/user_timeline.json?' +
-            'screen_name=realdonaldtrump' +
+            'screen_name=' + request.form['username'] +
             '&count=200' +
             '&include_rts=false'
         )
@@ -55,6 +55,11 @@ def tweets():
             current = vector.generateTransition((last, current))
             last = temp
     return return_text
+
+
+@app.route("/test")
+def test():
+    return "adsasdasdasd"
 
 
 @app.route("/")
--- static/app.jsx
+++ static/app.jsx
@@ -3,19 +3,42 @@ var UsernameInput = React.createClass({
     return {username: ''};
   },
   handleUsernameChange: function(e){
-    this.setState({username: e.targer.value});
+    this.setState({username: e.target.value});
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    this.state.data = 'Loading....';
+    var username = this.state.username.trim();
+    if(!username){
+      return;
+    }
+    $.ajax({
+      url: "http://localhost:5000/tweets",
+      type: 'POST',
+      data: {username: username},
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.error(status, err.toString());
+      }.bind(this)
+    });
+    this.setState({username: ''});
   },
   render: function(){
     return (
-      <form className="usernameForm">
-        <input
-          type="text"
-          placeholder="Twitter username"
-          value={this.state.username}
-          onChange={this.handleUsernameChange}
-        />
-      <input type="submit" value="Generate" />
-      </form>
+      <div>
+        <form className="usernameForm" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Twitter username"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+        <input type="submit" value="Generate" />
+        </form>
+        <p>{this.state.data}</p>
+    </div>
     );
   }
 });
